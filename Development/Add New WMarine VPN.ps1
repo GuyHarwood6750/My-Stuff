5ab6b37f586cf0c5dd07493f94d92cfb3902f6e8
Guy Harwood <accounts@harwood.co.za>
2019-04-05 17:39:57 +0200

    Modified

--- Development/Add New WMarine VPN.ps1
+++ Development/Add New WMarine VPN.ps1
@@ -1,3 +1,4 @@
 Get-VpnConnection -name 'WMarine VPN2' | out-file 'c:\test\vpndetails2.txt'
-Set-VpnConnection -Name 'WMarine VPN2' -ServerAddress '154.126.211.85'
+Set-VpnConnection -Name 'WMarine VPN2' -ServerAddress '41.75.108.157'
+Remove-VpnConnection -Name 'WMarine VPN2'
 #Add-VpnConnection -Name 'WMarine VPN2' -ServerAddress 'remote.circelaunches.co.za' -TunnelType 'sstp' -AuthenticationMethod 'mschapv2'
