ab894e0926dd782fba44af39c4229ca27e830b0e
Guy Harwood <accounts@harwood.co.za>
2019-04-13 18:24:55 +0200

    Init

---
+++ Daily Scripts/PetrolInvoices_1.ps1
@@ -0,0 +1,31 @@
+<#      Extract from Customer spreadsheet the range for new invoices to be generated.
+        Modify the $StartR (startrow) and $endR (endrow).
+        This can only be done by eyeball as spreadsheet has historical data.
+#>
+
+$inspreadsheet = 'C:\userdata\route 62\petrol books\PETROL BOOKS.xlsx'
+$csvfile = 'sheet1.csv'
+$pathout = 'C:\userdata\route 62\petrol books\'
+$custsheet = 'PA Paarl'                          #Customer worksheet
+$startR = 914                                    #Start row
+$endR = 917                                      #End Row
+$startCol = 1                                    #Start Col (don't change)
+$endCol = 9                                      #End Col (don't change)
+
+$b = $pathout + $csvfile
+
+Import-Excel -Path $inspreadsheet -WorksheetName $custsheet -StartRow $startR -StartColumn $startCol -EndRow $endR -EndColumn $endCol | Export-csv -Path $b -NoTypeInformation
+
+        Get-ChildItem -Path $pathout -Name $csvfile
+        $xl = New-Object -ComObject Excel.Application
+        $xl.Visible = $false
+        $xl.DisplayAlerts = $false
+        $wb = $xl.workbooks.Open($b)
+        $xl.Sheets.Item('sheet1').Activate()
+  
+        $range = $xl.Range("b:b").Entirecolumn
+        $range.NumberFormat = 'dd/mm/yyyy'
+
+        $wb.save()
+        $xl.Workbooks.Close()
+        $xl.Quit()
