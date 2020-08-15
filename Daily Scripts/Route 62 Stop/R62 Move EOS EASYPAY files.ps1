3f72bc7478847f033242fb90edde9572d0a80ca9
Guy Harwood <accounts@harwood.co.za>
2020-08-15 19:40:11 +0200

    x

--- Daily Scripts/Route 62 Stop/R62 Move EOS EASYPAY files.ps1
+++
@@ -1,9 +0,0 @@
-$src1 = 'C:\Users\Guy\Dropbox\R62\Accounts\EOS'
-$dest1 = 'C:\Userdata\Route 62\EOS Not Processed'
-$src2 = 'C:\Users\Guy\Dropbox\R62\Accounts\EASYPAY'
-$dest2 = 'C:\Userdata\Route 62\Easypay'
-$dest3 = 'C:\Userdata\Route 62\EOS Not Processed\Reports'
-
-Get-ChildItem -Path $src1\* -include *.txt | Move-Item -Destination $dest1 -Force
-Get-ChildItem -Path $src1\* -include *.pdf | Move-Item -Destination $dest3 -Force
-Get-ChildItem -Path $src2\* -include *.pdf | Move-Item -Destination $dest2 -Force
--- Daily Scripts/Route 62 Stop/R62S Compress Pastel data F2020.ps1
+++
@@ -1,9 +0,0 @@
-﻿#Backup Route 62 Stop current financial year Pastel files.
-
-Compress-Archive -Path "C:\Pastel18\R622021A" -DestinationPath "D:\Pastel Backups\Route 62 Stop\2021\R622021A $(get-date -f yyyyMMdd-HHmmss).zip" -force
-#
-#Compress-Archive -Path "C:\Pastel18\R622020A" -DestinationPath "C:\USERS\GUY\DROPBOX\R62\ACCOUNTS\PASTEL BACKUP\R622020A $(get-date -f yyyyMMdd-HHmmss).zip" -force
-<#
- Compress-Archive -Path "C:\Pastel18\R622020A" -DestinationPath "\\wserver\backup\PastelBKP\R622020A $(get-date -f yyyyMMdd-HHmmss).zip" -force   
-#>
-Write-EventLog -LogName MyPowerShell -Source "R62S" -EntryType Information -EventId 10 -Message "Archive of R622020A data completed"
